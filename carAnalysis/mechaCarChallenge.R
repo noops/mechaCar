df032f345440f9251355101c3dd74887225b77cd
Benjamin Corde Kirton <bkirton@benjamins-mbp-3.lan>
2020-09-13 13:52:52 -0500

    adding challenge files

--- carAnalysis/mechaCarChallenge.R
+++ carAnalysis/mechaCarChallenge.R
@@ -15,3 +15,22 @@ shapiro.test(mechaData$mpg)
 
 #check graph as well
 ggplot(mechaData, aes(mpg)) + geom_density()
+
+# multiple linear regression
+mechaMatrix <- as.matrix(mechaData[,c("vehicle length", "vehicle weight", "spoiler angle", "ground clearance", "mpg")])
+
+#calculate r using matrix
+cor(mechaMatrix)
+
+#variables of interest: vehicle length, ground clearance
+
+#create linear regression with variables of interest
+linReg <- lm(mpg ~ `vehicle length` + `ground clearance`,data = mechaData)
+
+summary(linReg)
+
+#create statistic summary for suspension data grouped by manufacturing lot
+suspensionSummary <- suspensionData %>% group_by(Manufacturing_Lot) %>% summarise(meanPSI = mean(PSI), medianPSI = median(PSI), varPSI = var(PSI), stdevPSI = sd(PSI))
+
+#perform t-test on suspension data
+t.test(suspensionData$PSI, mu=1500)
